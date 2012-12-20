fc3584a70de403921b6eaf39b6ac0c72b87805a9
Thomas Spycher <me@tspycher.com>
2012-12-20 07:24:26 +0100

    created two ways to create documents

--- src/products/document.py
+++ src/products/document.py
@@ -4,11 +4,13 @@ Created on Dec 19, 2012
 @author: thospy
 '''
 from libs import Database, Base, BaseModel
+from documentposition import Documentposition
+from product import Product
 
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref, deferred, validates
-#from sqlalchemy.sql import func
+from sqlalchemy.sql import func
 
 
 class Document(BaseModel, Base):
@@ -24,4 +26,8 @@ class Document(BaseModel, Base):
     type = Column(Integer)
     
     def sum(self):
-        pass
+        db = Database.instance()
+        return float(db.session.query(Documentposition, func.sum((Product.price/100*(100-Documentposition.deduction)) * Documentposition.quantity))
+            .join(Product)
+            .filter(Documentposition.document_id==self.id)
+            .all()[0][1])
--- test/producttests.py
+++ test/producttests.py
@@ -9,6 +9,7 @@ import os
 from libs import Database
 #from accounts import Account 
 from products import *
+#from sqlalchemy.sql import func
 
 class ProductsTest(unittest.TestCase):
 
@@ -26,20 +27,30 @@ class ProductsTest(unittest.TestCase):
 
 
     def test_createProduct(self):
-        product1 = Product(name="USB Stick", number="ABC-1234", price=12.50, description="Blubb")
-        product1.save()
         product2 = Product(name="USB Hub", number="ABC-1235", price=32.60, description="Blubb")
         product2.save()
         
+        ## Way 1 to add a new document
         document = Document(type=Document.BILL, customer=Customer(name="Customer GmbH"))
         document.save()
-        
-        documentposition1 = Documentposition(document_id=document.id, product_id=product1.id, deduction=0.0, quantity=2)
+         
+        documentposition1 = Documentposition(document_id=document.id, product=Product(name="USB Stick", number="ABC-1234", price=12.50, description="Blubb"), deduction=0.0, quantity=2)
         documentposition1.save()
-        
-        self.assertEqual(document.sum(), product1.price*2, "There is a wrong price %f" % product1.price*2)
-        
-        
+        documentposition2 = Documentposition(document_id=document.id, product=product2, deduction=0.0, quantity=3)
+        documentposition2.save()
+        sum = document.sum()
+        self.assertAlmostEqual(first=sum, second=122.8, msg="There is a wrong price %f not equal to %f" % (sum, float(122.80)))
+
+        ## Way 2 to add a new document
+        docs = [
+            Documentposition(product=Product(name="Harddisk", number="ABC-1236", price=129.90, description="Blubb"), deduction=10, quantity=1),
+            Documentposition(product=product2, deduction=0.0, quantity=1)
+        ]
+
+        document2 = Document(documentpositions=docs, type=Document.BILL, customer=Customer(name="Customer2 GmbH"))
+        document2.save()
+        sum = document2.sum()
+        self.assertAlmostEqual(first=sum, second=149.51, msg="There is a wrong price %f not equal to %f" % (sum,float(149.51)))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
